a7d2cb5727a0b4dfe1e73588c08b6f14cfdfc73f
theleiko <leiko@leiko.eu>
2025-07-25 21:27:19 +0200

    init

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+cp ./filesystem/opt /
+cp ./filesystem/boot /
+cp ./filesystem/root_init /
+
+apt-get update
+
+apt-get install -y fbi
+systemctl enable splashscreen.service
+systemctl disable getty@tty1
+
+
+remove_extra=$(remove_if_installed scratch squeak-plugins-scratch squeak-vm python-minecraftpi minecraft-pi sonic-pi oracle-java8-jdk bluej greenfoot libreoffice-common libreoffice-core freepats)
+
+apt-get remove -y --purge  $remove_extra
+
+apt-get autoremove -y
+
+#apt-get tools
+apt-get -y install git screen checkinstall avahi-daemon libavahi-compat-libdnssd1 xterm xdotool vim expect feh pulseaudio chromium x11vnc unclutter-xfixes
+
+sed -i 's@%BROWSER_START_SCRIPT%@/opt/custompios/scripts/start_chromium_browser@g' /opt/custompios/scripts/run_onepageos
+
+mkdir -p /opt/custompios/vnc
+chown kiosk:kiosk /opt/custompios/vnc
+
+echo "Please provide vnc password:"
+read line
+
+sudo -u pi /opt/custompios/scripts/setX11vncPass $line
+sync
+
+systemctl enable x11vnc.service
+
+apt-get clean
+apt-get autoremove -y
+
+chromium --app="https://extensions.gnome.org/extension/4099/no-overview/" &
