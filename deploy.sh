f856fc18eb95588f3e529ceca27972fbfed0171e
theleiko <leiko@leiko.eu>
2025-07-26 22:54:01 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 set -x
 set -e
+
+USER=$(id -nu 1000)
+
 
 cp -r filesystem/opt /
 cp -r filesystem/boot /
@@ -12,7 +21,7 @@ apt-get install -y fbi
 systemctl enable splashscreen.service
 systemctl disable getty@tty1
 
-sudo -u kiosk pcmanfm --set-wallpaper="/opt/custompios/background.png"
+sudo -u ${USER} pcmanfm --set-wallpaper="/opt/custompios/background.png"
 
 
 apt-get remove -y --purge  scratch squeak-plugins-scratch squeak-vm libreoffice-common libreoffice-core freepats
@@ -25,12 +34,12 @@ apt-get -y install git screen checkinstall avahi-daemon libavahi-compat-libdnssd
 sed -i 's@%BROWSER_START_SCRIPT%@/opt/custompios/scripts/start_chromium_browser@g' /opt/custompios/scripts/run_onepageos
 
 mkdir -p /opt/custompios/vnc
-chown kiosk:kiosk /opt/custompios/vnc
+chown ${USER}:${USER} /opt/custompios/vnc
 
 echo "Please provide vnc password:"
 read line
 
-sudo -u kiosk /opt/custompios/scripts/setX11vncPass $line
+sudo -u ${USER} /opt/custompios/scripts/setX11vncPass $line
 sync
 
 systemctl enable x11vnc.service
@@ -38,7 +47,9 @@ systemctl enable x11vnc.service
 apt-get clean
 apt-get autoremove -y
 
-sed -i 's/#type=local/autologin-user=kiosk\nautologin-user-timeout=0\n#type=local/g' /etc/lightdm/lightdm.conf 
+sed -i 's/#type=local/autologin-user=${USER}\nautologin-user-timeout=0\n#type=local/g' /etc/lightdm/lightdm.conf 
 
-echo 'export DISPLAY=:0.0' >> /home/kiosk/.profile
-echo '/opt/custompios/scripts/start_chromium_browser' >> /home/kiosk/.config/lxsession/LXDE/autostart
+sed -i 's/kiosk/${USER}/g' /etc/sudoers.d/kiosk 
+
+echo 'export DISPLAY=:0.0' >> /home/${USER}/.profile
+echo '/opt/custompios/scripts/start_chromium_browser' >> /home/${USER}/.config/lxsession/LXDE/autostart
